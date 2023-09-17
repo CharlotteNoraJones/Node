09af522ed61dc36ab3f2b739f24d008118ac11ed
CharlotteNoraJones <47127622+CharlotteNoraJones@users.noreply.github.com>
2023-09-17 17:56:22 -0500

    Basic server stuff.

--- serverTest/server.ts
+++ serverTest/server.ts
@@ -1,7 +1,44 @@
 import http from 'http';
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log('request made');
+    console.log(req.url, req.method);
+
+    // set header content type
+    res.setHeader('Content-Type', 'text/html');
+
+    let path: string = './views/';
+
+    switch(req.url) {
+     case '/':
+        path += 'index.html';
+        res.statusCode = 200;
+        break;
+    case '/about':
+        path += 'about.html';
+        res.statusCode = 200;
+        break;
+    case '/about-me':
+        path += 'about.html';
+        res.statusCode = 301;
+        break;
+    default: 
+        path += '404.html';
+        res.statusCode = 404;
+        break;
+    }
+
+    // send an html file
+    fs.readFile(path, (err: any, data: any) => {
+        if (err) {
+            console.log(err);
+            res.end();
+        } else {
+            // res.write(data)
+            res.statusCode = 200;
+            res.end(data);
+        }
+    });
 });
 
 server.listen(3000, 'localhost', () => {
